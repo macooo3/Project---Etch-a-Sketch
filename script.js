d6220c9e03283625c6d83b53a5515295a076ed05
macooo3 <marcoysu765@gmail.com>
2024-11-12 09:58:39 -0500

    added tiles

--- script.js
+++ script.js
@@ -11,19 +11,27 @@ const numGridRows = function (num) {
 };
 
 const boxes = function (num) {
-    // Get all grid-row elements after they're created
-    const gridRows = document.querySelectorAll(".grid-row");
-  
-    // Loop through each grid-row and add boxes inside it
-    gridRows.forEach((row) => {
-      for (let j = 0; j < num; j++) {
-        row.insertAdjacentHTML(
-          "beforeend",
-          `<div class="grid grid-${j}"></div>`
-        );
-      }
-    });
-  };
+  // Get all grid-row elements after they're created
+  const gridRows = document.querySelectorAll(".grid-row");
+
+  // Loop through each grid-row and add boxes inside it
+  gridRows.forEach((row) => {
+    for (let j = 0; j < num; j++) {
+      row.insertAdjacentHTML("beforeend", `<div class="grid grid-${j} color"></div>`);
+    }
+  });
+};
 
 numGridRows(10);
 boxes(10);
+
+const hover = function (mouse) {
+  if (mouse.target.classList.contains("grid")) {
+    const gridElement = mouse.target.closest('.grid');  // closest() checks ancestors for the class
+  
+    if (gridElement) {
+      gridElement.classList.add("color-black");
+    }
+  }
+};
+container.addEventListener("mouseover", hover);
